508e952b26b9e31e8cb2b6142642538b8c5a5af6
Evgeniy Alexeev <e.alekseev@k-soft-spb.ru>
2018-02-22 10:22:35 +0500

    added buttons and multy-user

--- ex_bot.py
+++ ex_bot.py
@@ -15,6 +15,7 @@ bot.
 from telegram import *
 from random import *
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from emoji import emojize
 import logging
 
 
@@ -23,30 +24,87 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-exersize_result=0
-exersize_str=""
+exercise_result=0
+exercise_str=""
+settings_dict={}
 
 
-def get_minus_exersize():
+def get_minus_exercise():
     a=1+int(random()*30)
     b=1+int(random()*(a-1))
     return a-b,str(a)+"-"+str(b)+"="
 
+def get_plus_exercise():
+    a=1+int(random()*30)
+    b=1+int(random()*(a-1))
+    return a+b,str(a)+"+"+str(b)+"="
+
+def new_question(bot,update):
+    check_new_chat(bot, update)
+    settings = settings_dict[update.message.chat.id]
+    settings["result"],settings["exercise_str"] = settings["func"]()
+    
+def ask_question(bot,update):
+    check_new_chat(bot, update)
+    settings = settings_dict[update.message.chat.id]
+    update.message.reply_text("Exercise: "+settings["exercise_str"])
+
+def check_result(bot,update):
+    if check_new_chat(bot, update):
+        ask_question(bot,update)
+    else:
+        settings = settings_dict[update.message.chat.id]
+        result_ok = False
+        try:
+            cur_answer=int(update.message.text)
+            if cur_answer==settings["result"]:result_ok=True
+        except: pass
+        if result_ok: 
+            update.message.reply_text("😺 Perfect!")
+            new_question(bot,update)
+        else:
+            update.message.reply_text("😿 Try again")
+        ask_question(bot,update)
+            
+    
+
+def send_keyboard(bot, update):
+    custom_keyboard = [[KeyboardButton(text="/Minus")],[KeyboardButton(text="/Plus")]]
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+    bot.send_message(chat_id=update.message.chat.id, text="Check an exercise", reply_markup=reply_markup)
+
+def check_new_chat(bot, update):
+    if update.message.chat.id not in settings_dict:
+        new_settings = {}
+        settings_dict[update.message.chat.id]=new_settings
+        new_settings["func"] = lambda: -1,"Please select exercise type!"
+        new_settings["result"]=0
+        new_settings["exercise_str"]=""
+        return True
+    return False
+    
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
 def start(bot, update):
     """Send a message when the command /start is issued."""
-    global exersize_result
-    global exersize_str
-    custom_keyboard = [[KeyboardButton(text="Minus")],[KeyboardButton(text="Plus")]]
-    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
-    bot.send_message(chat_id=update.message.chat.id, text="I'm back.", reply_markup=reply_markup)
-    exersize_result,exersize_str = get_minus_exersize()
-    update.message.reply_text("Пример: "+exersize_str)
+    global settings_dict    
+    send_keyboard(bot, update)
+    update.message.reply_text("Please select exercise type!")
+       
 
-def key_pressed(bot, update):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Key!')
+def plus_pressed(bot, update):
+    check_new_chat(bot, update)
+    settings = settings_dict[update.message.chat.id]
+    settings["func"] = get_plus_exercise 
+    new_question(bot,update)
+    ask_question(bot,update)
+
+def minus_pressed(bot, update):
+    check_new_chat(bot, update)
+    settings = settings_dict[update.message.chat.id]    
+    settings["func"] = get_minus_exercise
+    new_question(bot,update)
+    ask_question(bot,update)
 
 def help(bot, update):
     """Send a message when the command /help is issued."""
@@ -55,21 +113,10 @@ def help(bot, update):
 
 def echo(bot, update):
     """Echo the user message."""
-    global exersize_result,exersize_str
-    local_result=0
-    try:
-        local_result=int(update.message.text)
-    except:
-        local_result=0        
-        
-    if exersize_result==local_result:
-        update.message.reply_text("Правильно, молодец! :)")
-        exersize_result,exersize_str = get_minus_exersize()
-        update.message.reply_text("Пример: "+exersize_str)
-        
+    if check_new_chat(bot, update):
+        ask_question(bot,update)
     else:
-        update.message.reply_text("Неправильно! :(")
-        update.message.reply_text("Попробуй ещё: "+exersize_str)
+           check_result(bot,update)
 
 
 def error(bot, update, error):
@@ -87,7 +134,8 @@ def main():
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler('key', key_pressed))
+    dp.add_handler(CommandHandler('Plus', plus_pressed))
+    dp.add_handler(CommandHandler('Minus', minus_pressed))
     dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
